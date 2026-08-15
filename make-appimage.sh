9df8c91900096943f8d63190f0cee4f3a6696d27
assem <77121724+assem11assem@users.noreply.github.com>
2026-08-15 15:45:49 +0300

    Update make-appimage.sh
    FEXBash needs bash so, I added it to quick-sharun

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,7 +16,7 @@ export DEPLOY_PIPEWIRE=1
 export DEPLOY_PYTHON=1 #Because Why not.
 
 # Deploy dependencies
-quick-sharun /bin/FEX* /usr/bin/FEX* /usr/bin/curl /usr/bin/unsquashfs /usr/bin/squashfuse /usr/bin/*erofs* /bin/*erofs* /usr/lib/libFEXCore.so /usr/share/fex-emu/* /usr/lib/*FEX* #/usr/share/binfmts/FEX* /var/lib/binfmts/FEX*
+quick-sharun /bin/FEX* /usr/bin/FEX* /bin/bash /usr/bin/curl /usr/bin/unsquashfs /usr/bin/squashfuse /usr/bin/*erofs* /bin/*erofs* /usr/lib/libFEXCore.so /usr/share/fex-emu/* /usr/lib/*FEX* #/usr/share/binfmts/FEX* /var/lib/binfmts/FEX*
 #echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
